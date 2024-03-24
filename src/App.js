305b74c873a8207e2aa07f0e48dcdfd1dce538b6
prince <prince.dwivedi91@gmail.com>
2024-03-24 22:44:12 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import './App.css';
 import Welcome from '../src/component/welcome';
 
 const App = () => {
-  // const handleIntroductionSubmit = (name) => {
-  //   console.log(`Hello, ${name}!`);
-  //   // You can add logic here to update the user table in the FitGlide database
-  // };
+  
 
   return (
     <div className="App">
